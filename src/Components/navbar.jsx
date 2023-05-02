80a6a743645841d9d9afb16bb48443eacc5f928d
acastagnino96 <109534721+acastagnino96@users.noreply.github.com>
2023-05-01 22:21:29 -0300

    Update navbar.jsx

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -9,7 +9,7 @@ function BasicExample() {
     <Navbar className="nvbar sticky-top" expand="lg">
       <Container>
         <Navbar.Brand as={Link} to="/" className="d-flex align-items-center">
-          <img src="../src/assets/logo.png"  height={105}/>
+          <img src="https://firebasestorage.googleapis.com/v0/b/a-cara-de-perro-5251e.appspot.com/o/logo.png?alt=media&token=149cf83b-bb52-4e73-8081-369d08519c94"  height={105}/>
           <div className="logoTexto">
             <p>A CARA DE PERRO <span className="d-block" id="logoDescription">PET SHOP & PELUQUERIA CANINA</span></p>
           </div>
