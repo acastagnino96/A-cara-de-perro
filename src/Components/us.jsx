6c71b82b8fdb0bf0d3b734a45df176f702052852
acastagnino96 <109534721+acastagnino96@users.noreply.github.com>
2023-05-01 22:22:01 -0300

    Update us.jsx

--- src/Components/us.jsx
+++ src/Components/us.jsx
@@ -5,7 +5,7 @@ export default function Us() {
     return (
         <section>
             <div className="usBox">
-                <img src="../src/assets/fototienda.jpg" alt="fototienda" />
+                <img src="https://firebasestorage.googleapis.com/v0/b/a-cara-de-perro-5251e.appspot.com/o/fototienda.jpg?alt=media&token=e9a8bb08-0946-47c3-85d0-fc6af83391de" alt="fototienda" />
                 <div className="textUs">
                         <h1>A CARA DE PERRO <span className="d-block" id="textDescription">PET SHOP & PELUQUERIA CANINA</span></h1>
                         <p>Somos una tienda de productos para mascotas que comercializamos alimentos de todo tipo para perros y gatos, tenemos productos para la higiene y accesorios muy variados, peceras, caniles, bolsos transportadores, colchonetas, ropa de diseño y mucho más. <br />¡Además realizamos envíos a domicilio! <br /><br />Contamos además con nuestro sector de Peluquería Canina en donde tu perro podrá disfrutar de un delicado baño y un excelente trato. Nos especializamos en cortes de raza, limpieza de oídos y corte de uñas gracias a nuestros profesionales más capacitados en un salón vidriado con vista a la calle para tu tranquilidad. <br /><br />¡Te esperamos en nuestras sucursales!</p>
